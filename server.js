114fca78c8b12073d8b6e83d68b94e07389a20e4
julioneguinho-code <julioneguinho@hotmail.com>
2026-08-28 11:50:36 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -174,6 +174,9 @@ const server = http.createServer(async (req, res) => {
       if (p === '/api/gestor/vendas-log' && req.method === 'GET') {
         return send(res, 200, await notion.listarVendasLog(session, { limite: url.searchParams.get('limite') }));
       }
+      if (p === '/api/gestor/negociacoes-todas' && req.method === 'GET') {
+        return send(res, 200, await notion.negociacoesTodasEquipes(session));
+      }
       if (p === '/api/gestor/clientes' && req.method === 'GET') {
         return send(res, 200, await notion.listarClientes(session, { q: url.searchParams.get('q'), consultor: url.searchParams.get('consultor') }));
       }
